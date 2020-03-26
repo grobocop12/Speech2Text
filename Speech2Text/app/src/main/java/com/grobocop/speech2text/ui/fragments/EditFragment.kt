702034b403314639fe026c248722f58b51c89e7e
grobocop <chasnik97876@gmail.com>
2020-03-26 23:02:53 +0100

    Title TextView
    Added TextView for title in EditFragment

--- Speech2Text/app/src/main/java/com/grobocop/speech2text/ui/fragments/EditFragment.kt
+++ Speech2Text/app/src/main/java/com/grobocop/speech2text/ui/fragments/EditFragment.kt
@@ -1,28 +1,23 @@
 package com.grobocop.speech2text.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
 import com.grobocop.speech2text.R
 import com.grobocop.speech2text.data.Transcription
 import com.grobocop.speech2text.ui.viewModel.TranscriptionViewModel
+import com.grobocop.speech2text.utils.SpeechRecognizerObserver
 import com.grobocop.speech2text.utils.SpeechRecognizerProvider
 import java.util.*
-import com.grobocop.speech2text.utils.SpeechRecognizerObserver as SpeechRecognizerObserver
 
 class EditFragment : Fragment() {
     private lateinit var editViewModel: TranscriptionViewModel
@@ -32,7 +27,6 @@ class EditFragment : Fragment() {
     private var speechRecognizer: SpeechRecognizer? = null
     private var id: Int? = null
     private var isListening = false
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,7 +57,6 @@ class EditFragment : Fragment() {
                 transcriptionET.setText(it.text)
             })
         }
-
     }
 
     private fun setUI(root: View) {
@@ -79,29 +72,28 @@ class EditFragment : Fragment() {
                 speechRecognizer?.stopListening()
                 fab.setImageDrawable(resources.getDrawable(R.drawable.ic_fiber_record_white, theme))
                 lastWordTV.text = ""
+                isListening = false
             } else {
-                val observer = getRecognizerObserver()
                 speechRecognizer = SpeechRecognizerProvider.createSpeechRecognizer(
                     this.activity?.applicationContext,
-                    observer
+                    getRecognizerObserver()
                 )
                 val intent = SpeechRecognizerProvider.createSpeechRecognitionIntent()
                 speechRecognizer?.startListening(intent)
                 fab.setImageDrawable(resources.getDrawable(R.drawable.ic_stop_white, theme))
+                isListening = true
             }
-            isListening = !isListening
+
         }
     }
 
     private fun getRecognizerObserver(): SpeechRecognizerObserver {
         return object : SpeechRecognizerObserver {
-
             override fun onResult(result: String?) {
                 result.let {
                     val text = transcriptionET.text.toString() + " " + it
                     transcriptionET.setText(text)
                 }
-
             }
 
             override fun onReadyForSpeech() {
@@ -122,7 +114,6 @@ class EditFragment : Fragment() {
             }
 
             override fun onStop() {
-
                 fab.setImageDrawable(
                     resources.getDrawable(
                         R.drawable.ic_fiber_record_white,
@@ -133,25 +124,5 @@ class EditFragment : Fragment() {
                 isListening = false
             }
         }
-    }
-
-    private fun setUpIntent(): Intent {
-        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        intent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-        )
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
-        intent.putExtra(
-            RecognizerIntent.EXTRA_PROMPT,
-            "Recognizer will close automatically after "
-                    + 500
-                    + " milliseconds of complete silence."
-        )
-        intent.putExtra(
-            RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 500
-        )
-        return intent
     }
 }
